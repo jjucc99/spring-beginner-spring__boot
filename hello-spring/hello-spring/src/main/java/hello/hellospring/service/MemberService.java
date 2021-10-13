74e1bf1076fb6159b116fdf537f40c19e8edc7ef
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2021-10-13 19:51:44 +0900

    스프링 빈과 의존관계

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,9 +3,12 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public class MemberService {
     //  객체를 생성할 때 구현체를 넣어줘야 하는 관계 => dependence injection ===  DI
