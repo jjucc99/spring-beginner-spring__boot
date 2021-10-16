1d6537ffcea528b5752bad58a94d4663c50a74b5
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2021-10-16 15:15:21 +0900

    finally chapter clear

--- hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,13 +1,17 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
 
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
     //  객체를 생성할 때 구현체를 넣어줘야 하는 관계 => dependence injection ===  DI
     private final MemberRepository memberRepository;
